8809f2c4379f8610bb9ea9bb8759321b1e9fde10
Gustavo Vinicius <guvinidantas@gmail.com>
2022-12-30 17:49:42 -0300

    fix tests for test services

--- src/api/user/repositories/fakes/UserFakeRepository.ts
+++ src/api/user/repositories/fakes/UserFakeRepository.ts
@@ -1,20 +1,27 @@
 import { IUser } from "../../entities/interface/IUser";
 import { faker } from "@faker-js/faker";
+import { IUserRepository } from "../interfaces/IUserRepository";
 
-export class UserFakeRepository {
-  public listAll(): IUser[] {
-    const users: IUser[] = [];
-    for (let i = 0; i < 1; i++) {
-      users.push({
-        name: faker.name.firstName(),
-        email: faker.internet.email(),
-      });
-    }
+export class UserFakeRepository implements IUserRepository {
+  public listAll(): Promise<IUser[]> {
+    const users = new Promise<IUser[]>((resolve, _) => {
+      const userArray: IUser[] = [];
+      for (let i = 0; i < 1; i++) {
+        userArray.push({
+          name: faker.name.firstName(),
+          email: faker.internet.email(),
+        });
+      }
+      return resolve(userArray);
+    });
     return users;
   }
 
-  public create(): IUser {
-    const user: IUser = { name: faker.name.firstName(), email: faker.internet.email() };
+  public create(): Promise<IUser> {
+    const user = new Promise<IUser>((resolve, _) => {
+      const user = { name: faker.name.firstName(), email: faker.internet.email() };
+      return resolve(user);
+    });
     return user;
   }
 }
--- src/api/user/services/tests/userServices.spec.ts
+++ src/api/user/services/tests/userServices.spec.ts
@@ -1,15 +1,22 @@
-import { IUser } from "../../entities/interface/IUser";
+import { faker } from "@faker-js/faker";
 import { UserFakeRepository } from "../../repositories/fakes/UserFakeRepository";
+import { CreateUserService } from "../CreateUserService";
+import { ListUserService } from "../ListUserService";
 
 describe("Tests about all user services", () => {
-  it("Should be able list all users", () => {
+  it("Should be able list all users", async () => {
     const fakeRepository = new UserFakeRepository();
-    const users: IUser[] = fakeRepository.listAll();
+    const listUserService = new ListUserService(fakeRepository);
+    const users = await listUserService.execute();
     expect(users).toHaveLength(1);
   });
-  it("Shoud be able create a new user", () => {
+  it("Shoud be able create a new user", async () => {
     const fakeRepository = new UserFakeRepository();
-    const user: IUser = fakeRepository.create();
+    const createUserService = new CreateUserService(fakeRepository);
+    const user = await createUserService.execute({
+      name: faker.name.firstName(),
+      email: faker.internet.email(),
+    });
     expect(user).toHaveProperty("name");
   });
 });
