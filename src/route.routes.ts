787160403b19c0970227168757addebb025132fa
Gustavo Vinicius <guvinidantas@gmail.com>
2022-12-30 15:15:22 -0300

    send message hello dev and try remove coverage folder

--- src/route.routes.ts
+++ src/route.routes.ts
@@ -3,5 +3,5 @@ import { Request, Response, Router } from "express";
 export const appRoutes = Router();
 
 appRoutes.get("/", (_: Request, res: Response) => {
-  res.status(200).send();
+  res.status(200).send({ message: "hello dev!" });
 });
